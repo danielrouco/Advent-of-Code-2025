cd26cc48f2a6054b264f720ac5238886c6830a80
danielrouco <danielrouco188rr@gmail.com>
2026-01-17 13:56:36 +0100

    day4 part1

---
+++ day4/part1.ml
@@ -0,0 +1,40 @@
+let file = open_in "input.txt"
+
+let map =
+  let rec aux () =
+    try
+      let str = input_line file in
+      Array.init (String.length str) (fun x -> String.get str x = '@') :: aux ()
+    with End_of_file -> []
+  in
+  Array.of_list (aux ())
+
+let surroundings i j =
+  [
+    (i - 1, j - 1);
+    (i - 1, j);
+    (i - 1, j + 1);
+    (i, j - 1);
+    (i, j + 1);
+    (i + 1, j - 1);
+    (i + 1, j);
+    (i + 1, j + 1);
+  ]
+
+let accesible i j map =
+  List.fold_left
+    (fun acc (x, y) ->
+      acc + try if map.(x).(y) then 1 else 0 with Invalid_argument _ -> 0)
+    0 (surroundings i j)
+  < 4
+
+let result =
+  let n = ref 0 in
+  for i = 0 to Array.length map - 1 do
+    for j = 0 to Array.length map.(0) - 1 do
+      if map.(i).(j) && accesible i j map then incr n else ()
+    done
+  done;
+  !n
+
+let _ = print_endline (string_of_int result)
